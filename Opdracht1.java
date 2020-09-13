0c922cdc0f0d687a5822c71ad28a5b7ede3ed12c
Franck Kamenan <franck.kamenan@yahoo.com>
2020-09-13 12:04:24 +0200

    Suite Opdracht1

--- Opdracht1.java
+++ Opdracht1.java
@@ -1,15 +1,23 @@
 package be.intecbrussel.opdrachten;
 
 import java.io.IOException;
+import java.nio.charset.Charset;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
+import java.nio.file.StandardOpenOption;
+import java.util.ArrayList;
+import java.util.List;
 
 public class Opdracht1 {
 
     public static void main(String[] args) {
 
         Path destDir = Paths.get("c:/Data");
+        Path destFile = destDir.resolve("test.txt");
+        List<String> content = new ArrayList<>();
+        content.add("Some lines");
+        content.add(" are added.");
 
         try{
             Files.createDirectories(destDir);
@@ -18,13 +26,19 @@ public class Opdracht1 {
         }
 
         try{
-            if (Files.notExists(destDir.resolve("test.txt"))) {
-                Files.createFile(destDir.resolve("test.txt"));
+            if (Files.notExists(destFile)) {
+                Files.createFile(destFile);
                 System.out.println("Files is created!");
             }
             else {
                 System.out.println("File Already Exists.");
             }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        try {
+            Files.write(destFile, content, Charset.defaultCharset(), StandardOpenOption.CREATE);
         } catch (IOException e) {
             e.printStackTrace();
         }
