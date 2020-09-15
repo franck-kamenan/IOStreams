417111e76fde19fd42931a56b3219fea6e941dd6
Franck Kamenan <franck.kamenan@yahoo.com>
2020-09-15 13:36:17 +0200

    toFile And toPath

--- io_streams/character_streams/WriteFileApp.java
+++ io_streams/character_streams/WriteFileApp.java
@@ -1,13 +1,24 @@
 package be.intecbrussel.opdrachten.io_streams.character_streams;
 
+import java.io.File;
 import java.io.FileWriter;
 import java.io.IOException;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 
 public class WriteFileApp {
 
     public static void main(String[] args) throws IOException {
 
-        try(FileWriter fWriter = new FileWriter("MyTextFile.txt");
+        Path pathTotallyNotNeededAtAll = Paths.get("MyTextFile.txt");
+        File file = pathTotallyNotNeededAtAll.toFile();
+        Path copy = file.toPath();
+
+        System.out.println(pathTotallyNotNeededAtAll);
+        System.out.println(file);
+        System.out.println(copy);
+
+        try(FileWriter fWriter = new FileWriter(pathTotallyNotNeededAtAll.toFile());
             FileWriter fileWriter = new FileWriter("SecondFile.txt");) {
 
             fWriter.write("The ");
