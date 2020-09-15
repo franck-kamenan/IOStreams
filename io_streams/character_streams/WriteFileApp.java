5730e096e074416834adeb3f71bc6ca14c66c92e
Franck Kamenan <franck.kamenan@yahoo.com>
2020-09-15 11:32:17 +0200

    throws IOException

--- io_streams/character_streams/WriteFileApp.java
+++ io_streams/character_streams/WriteFileApp.java
@@ -5,14 +5,14 @@ import java.io.IOException;
 
 public class WriteFileApp {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
 
+        final FileWriter fWriter = new FileWriter("MyTextFile.txt");
 
+        try(fWriter) {
 
-        try(FileWriter fWriter = new FileWriter("MyTextFile.txt")) {
-
-            fWriter.write("Hello ");
-            fWriter.write("again!");
+            fWriter.write("The ");
+            fWriter.write("other way!");
 
         } catch (IOException e) {
             e.printStackTrace();
