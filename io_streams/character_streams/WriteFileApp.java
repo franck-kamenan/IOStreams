d8d8ba9d00a22a723f9e3943a6ac2986a815ee4b
Franck Kamenan <franck.kamenan@yahoo.com>
2020-09-15 11:23:05 +0200

    Hello again

--- io_streams/character_streams/WriteFileApp.java
+++ io_streams/character_streams/WriteFileApp.java
@@ -7,27 +7,16 @@ public class WriteFileApp {
 
     public static void main(String[] args) {
 
-        FileWriter fWriter = null;
 
-        try {
-            fWriter = new FileWriter("MyTextFile.txt");
 
-            fWriter.write("Hello");
-            fWriter.write("World!");
+        try(FileWriter fWriter = new FileWriter("MyTextFile.txt")) {
+
+            fWriter.write("Hello ");
+            fWriter.write("again!");
 
         } catch (IOException e) {
             e.printStackTrace();
 
-        } finally {
-
-            try {
-                if (fWriter != null) {
-                    fWriter.close();
-                }
-            }catch (IOException io){
-                System.out.println("Error closing a filewriter");
-            }
-            System.out.println("Job done, no problem sir!");
         }
     }
 }
